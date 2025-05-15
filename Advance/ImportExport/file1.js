302d0a8a5c6abc033cde704c7d1febb3feb1c049
Mohit Kumar <mks.djcet@gmail.com>
2025-05-15 16:10:39 +0530

    Updated code

---
+++ Advance/ImportExport/file1.js
@@ -0,0 +1,7 @@
+// import {name, add} from './file2.js'; // Importing from file2.js
+
+// console.log(name); // Output: Mohit Decodes
+// console.log(add(5, 10)); // Output: 15
+
+import addxyz from './file2.js'; // Importing default export from file2.js
+console.log(addxyz(10, 10)); // Output: 15
---
+++ Advance/ImportExport/file2.js
@@ -0,0 +1,10 @@
+//Export from Modules
+// export const name = "Mohit Decodes";
+// export function add(a, b) {
+//   return a + b;
+// }
+
+//Exporting default
+export default function add(a, b) {
+  return a + b;
+}
